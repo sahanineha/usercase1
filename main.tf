0bbcdba185804b30751be5235708eafad3cc0f09
sahanineha <sahanineha9087@gmail.com>
2025-06-02 11:29:32 +0000

    2

--- main.tf
+++ main.tf
@@ -76,6 +76,19 @@ provider "aws" {
 
 ##################################################
 
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ] # GitHub's known root CA fingerprint
+}
+
 data "aws_iam_policy_document" "github_oidc_assume_role" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -83,7 +96,8 @@ data "aws_iam_policy_document" "github_oidc_assume_role" {
     principals {
       type        = "Federated"
       identifiers = [
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+           aws_iam_openid_connect_provider.github.arn
+ /*       "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com" */
       ]
     }
 
